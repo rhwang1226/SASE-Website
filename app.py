a4d4b729c797f2a9570108562f953d1c1064c43b
plaine1226 <61886677+plaine1226@users.noreply.github.com>
2023-06-11 04:05:58 -0400

    admin login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g, jsonify, send_from_directory
+from flask import Flask, render_template, request, g, jsonify, send_from_directory, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from sqlite3 import Error
@@ -30,6 +30,21 @@ def eboard():
 
     con.close()
     return render_template('eboard.html',eboardRows=eboardRows)
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/admin/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('admin'))
+    return render_template('adminLogin.html', error=error)
 
 # Run the Flask application
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -5,11 +5,13 @@ print("Connected to database successfully")
 
 con.execute('DROP TABLE IF EXISTS eboard')
 
-con.execute('CREATE TABLE eboard (name TEXT UNIQUE CHECK(name <> ""), position TEXT NOT NULL, greeting TEXT NOT NULL CHECK(greeting <> ""))')
+con.execute('CREATE TABLE eboard (name TEXT NOT NULL CHECK(name <> ""), position TEXT NOT NULL, greeting TEXT NOT NULL CHECK(greeting <> ""))')
+con.execute('CREATE TABLE login (username TEXT NOT NULL CHECK(name <> ""), password TEXT NOT NULL')
 print("Created table successfully!")
 
 cur = con.cursor()
-cur.execute("INSERT INTO eboard (name, position, greeting) VALUES ('Hannah Kim', 'Programming Language', 'Hannah Kim is a Junior majoring in Microbiology! With a passion for exploring the intricacies of the microscopic world, Hannah is a dedicated student on her academic journey. When she''s not immersed in her scientific studies, you can find her indulging in her favorite KDramas, currently relishing the joys of rewatching Hospital Playlist Season 2. Additionally, Hannah finds solace in the captivating world of books, spending her free time engrossed in the pages of various literary adventures. As a leisure activity, she also enjoys hitting the golf course, honing her skills and enjoying the outdoors. Now, here''s an interesting tidbit about Hannah: despite her time on campus, she has never set foot on North Campus and has yet to experience the thrill of riding a Blue Bus.')")
+cur.execute("INSERT INTO eboard (name, position, greeting) VALUES ('Hannah Kim', 'President', 'Hannah Kim is a Junior majoring in Microbiology! With a passion for exploring the intricacies of the microscopic world, Hannah is a dedicated student on her academic journey. When she''s not immersed in her scientific studies, you can find her indulging in her favorite KDramas, currently relishing the joys of rewatching Hospital Playlist Season 2. Additionally, Hannah finds solace in the captivating world of books, spending her free time engrossed in the pages of various literary adventures. As a leisure activity, she also enjoys hitting the golf course, honing her skills and enjoying the outdoors. Now, here''s an interesting tidbit about Hannah: despite her time on campus, she has never set foot on North Campus and has yet to experience the thrill of riding a Blue Bus.')")
+
 con.commit()
 
 con.close()
