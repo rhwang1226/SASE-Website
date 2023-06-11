481b2c9b7f1ccc208e8297aba2fbd12cb1eacc67
plaine1226 <61886677+plaine1226@users.noreply.github.com>
2023-06-11 03:07:05 -0400

    commiting html code no jinja with flask

---
+++ database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+con = sqlite3.connect('database.db')
+print("Connected to database successfully")
+
+con.execute('DROP TABLE IF EXISTS eboard')
+
+con.execute('CREATE TABLE eboard (name TEXT UNIQUE CHECK(name <> ""), position TEXT NOT NULL, greeting TEXT NOT NULL CHECK(greeting <> ""))')
+print("Created table successfully!")
+
+cur = con.cursor()
+cur.execute("INSERT INTO eboard (name, position, greeting) VALUES ('Hannah Kim', 'Programming Language', 'Hannah Kim is a Junior majoring in Microbiology! With a passion for exploring the intricacies of the microscopic world, Hannah is a dedicated student on her academic journey. When she''s not immersed in her scientific studies, you can find her indulging in her favorite KDramas, currently relishing the joys of rewatching Hospital Playlist Season 2. Additionally, Hannah finds solace in the captivating world of books, spending her free time engrossed in the pages of various literary adventures. As a leisure activity, she also enjoys hitting the golf course, honing her skills and enjoying the outdoors. Now, here''s an interesting tidbit about Hannah: despite her time on campus, she has never set foot on North Campus and has yet to experience the thrill of riding a Blue Bus.')")
+con.commit()
+
+con.close()
--- main.py
+++ main.py
@@ -15,6 +15,22 @@ app.static_folder = 'static'
 def index():
     return render_template('index.html')
 
+@app.route('/eboard')
+def eboard():
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
+    
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM eboard")
+
+    rows = cur.fetchall()
+    eboardRows = [{"name": row[0], "position": row[1], "greeting": row[2]} for row in rows]
+    
+
+    con.close()
+    return render_template('eboard.html',eboardRows=eboardRows)
+
 # Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
